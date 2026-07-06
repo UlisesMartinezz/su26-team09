323bd0d27af7243518672d4b31c712b4c1b319d6
BryanMartinez-Bautista <bryanmartinez1283@gmail.com>
2026-07-06 11:07:55 -0400

    Added backend api files

---
+++ backend-api/src/test/java/com/CSC340/ClipzConnect/ClipzConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC340.ClipzConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipzConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
