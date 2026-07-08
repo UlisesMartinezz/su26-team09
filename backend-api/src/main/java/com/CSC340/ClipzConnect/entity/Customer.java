9eed0b70f32f77279cdf57d8c11fa7177d94c1ac
BryanMartinez-Bautista <bryanmartinez1283@gmail.com>
2026-07-07 22:43:43 -0400

    Added Customer entity code

--- backend-api/src/main/java/com/CSC340/ClipzConnect/entity/Customer.java
+++ backend-api/src/main/java/com/CSC340/ClipzConnect/entity/Customer.java
@@ -1,5 +1,64 @@
 package com.CSC340.ClipzConnect.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String accountStatus;
     
+    @Column(nullable = false)
+    private String location;
+
+    public Customer(String name, String email, String phoneNumber, String password, String accountStatus, 
+        String location){
+            this.name = name;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            this.password = password;
+            this.accountStatus = accountStatus;
+            this.location = location;
+    }
+
+    @OneToMany(mappedBy = "customer")
+    @JsonIgnoreProperties({"customer"})
+    private List<HairAppointment> hairAppointments;
+
+    @OneToMany(mappedBy = "customer")
+    @JsonIgnoreProperties({"customer"})
+    private List<Review> reviews;
 }
